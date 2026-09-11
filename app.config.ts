7867081a4ea6d5c736d94be2e12cbb92c729bb4c
Kesar Saleem <kesarsaleem81@gmail.com>
2026-09-11 15:44:48 +0500

    commit

--- app.config.ts
+++ app.config.ts
@@ -7,9 +7,16 @@ const config: ExpoConfig = {
   version: '1.0.0',
   orientation: 'portrait',
   userInterfaceStyle: 'automatic',
+  icon: './assets/images/app_icon.png',
   plugins: ['expo-router', 'expo-secure-store'],
   ios: { supportsTablet: false, bundleIdentifier: 'com.interviewpulse.app' },
-  android: { package: 'com.interviewpulse.app' },
+  android: {
+    package: 'com.interviewpulse.app',
+    adaptiveIcon: {
+      foregroundImage: './assets/images/app_icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
   extra: {
     supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
